cf59a5a5a631bfd5ce23c70b2a99cd8e6b929cd1
teddy edmond <92273094+Redhewlett@users.noreply.github.com>
2022-10-28 16:46:21 +0200

    search section done?

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// ---------- header Scroll ----------
 const header = document.querySelector('.mainHeader')
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 200) {
@@ -6,3 +7,22 @@ window.onscroll = function () {
     header.classList.remove('mainHeader--grown')
   }
 }
+// ---------- header Scroll ----------
+
+// ---------- toogle button ----------
+const text = document.querySelector('.withShipping__text')
+const btn = document.querySelector('.withShipping__btnContainer')
+const toggle = document.getElementById('withShipping__btn')
+const shippingArray = [text, btn]
+for (let i = 0; i < shippingArray.length; i++) {
+  shippingArray[i].addEventListener('click', () => {
+    if (toggle.className != 'withShipping__btn--active') {
+      toggle.classList.add('withShipping__btn--active')
+      toggle.innerHTML = '<i class="fa-solid fa-check"></i>'
+    } else {
+      toggle.classList.remove('withShipping__btn--active')
+      toggle.innerHTML = '<i class="fa-solid fa-x"></i>'
+    }
+  })
+}
+// ---------- toogle button ----------
