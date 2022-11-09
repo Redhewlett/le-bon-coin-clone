d284ac67bb04fa781fc96e1b4ce8752a09d94209
teddy edmond <92273094+Redhewlett@users.noreply.github.com>
2022-11-09 17:33:38 +0100

    working on category menu
    menu opening and closing on click (button or copycat button inside the menu)
    todo: need to work on the closing when clicking outside

--- index.js
+++ index.js
@@ -80,3 +80,155 @@ countryList.addEventListener('mouseleave', () => {
   countryList.classList.toggle('hidden')
 })
 // ------ country selector end -------
+
+// ------ category list box ------
+const categoriesBtn = document.querySelector('.category--button')
+const refBox = document.querySelector('.search_tool')
+let isOpen = false
+// array of categories
+const CategoriesArray = [
+  {
+    title: 'Vacances',
+    icon: '<i class="fa-solid fa-sun"></i>',
+    subCategories: ['Locations & gîtes', "Chambres d'hôtes", 'Campings', 'Hébergements insolites', 'hôtels', 'Ventes privées vacances', 'Locations en Espagne']
+  },
+  {
+    title: 'Emploi',
+    icon: '<i class="fa-solid fa-briefcase"></i>',
+    subCategories: ["Offres d'emploi", "Offres d'emploi Cadres", 'Formations Professionnelles']
+  },
+  {
+    title: 'Véhicules',
+    icon: '<i class="fa-solid fa-car-rear"></i>',
+    subCategories: [
+      'Voitures',
+      'Motos',
+      'Caravaning',
+      'Utilitaires',
+      'Camions',
+      'Nautisme',
+      'Équipement auto',
+      'Équipement moto',
+      'Équipement caravaning',
+      'Équipement nautisme'
+    ]
+  },
+  {
+    title: 'Immobilier',
+    icon: '<i class="fa-solid fa-house"></i>',
+    subCategories: ['Ventes immobilières', 'Immobilier Neuf', 'Locations', 'Colocations', 'Bureaux & Commerces']
+  },
+  {
+    title: 'Mode',
+    icon: '<i class="fa-solid fa-shirt"></i>',
+    subCategories: ['Vêtements', 'Chaussures', 'Accessoires & Bagagerie', 'Montres & Bijoux', 'Équipement bébé', 'Vêtements bébé', 'Luxe et Tendance']
+  },
+  {
+    title: 'Maison',
+    icon: '<i class="fa-solid fa-couch"></i>',
+    subCategories: ['Ameublement', 'Électroménager', 'Arts de la table', 'Décoration', 'Linge de maison', 'Bricolage', 'Jardinage']
+  },
+  {
+    title: 'Multimédia',
+    icon: '<i class="fa-solid fa-mobile-screen-button"></i>',
+    subCategories: ['Informatique', 'Consoles & Jeux vidéo', 'Image & Son', 'Téléphonie']
+  },
+  {
+    title: 'Loisir',
+    icon: '<i class="fa-solid fa-basketball"></i>',
+    subCategories: [
+      'DVD - Films',
+      'CD - Musique',
+      'Livres',
+      'Vélos',
+      'Sports & Hobbies',
+      'Instruments de musique',
+      'Collection',
+      'Jeux & Jouets',
+      'Vins & Gastronomie'
+    ]
+  },
+  {
+    title: 'Animaux',
+    icon: '<i class="fa-solid fa-paw"></i>',
+    subCategories: ['Animaux']
+  },
+  {
+    title: 'Materiel Professionnel',
+    icon: '<i class="fa-solid fa-dolly"></i>',
+    subCategories: [
+      'Matériel agricole',
+      'Transport - Manutention',
+      'BTP - Chantier gros-oeuvre',
+      'Outillage - Matériaux 2nd-oeuvre',
+      'Équipements industriels',
+      'Restauration - Hôtellerie',
+      'Fournitures de bureau',
+      'Commerces & Marchés',
+      'Matériel médical'
+    ]
+  },
+  {
+    title: 'Services',
+    icon: '<i class="fa-solid fa-handshake-angle"></i>',
+    subCategories: ['Prestations de services', 'Billetterie', 'Évènements', 'Cours particuliers', 'Covoiturage']
+  },
+  {
+    title: 'Autres',
+    icon: '<i class="fa-solid fa-ellipsis"></i>',
+    subCategories: ['Autres']
+  }
+]
+//the html to inject
+const listHtml = `
+<span id='category__box' class='category__box'>
+  <div class='category__box__list'>
+    <div id='categoryBtnCopy' class='category__box__list__element'>
+        <i class="fa-solid fa-list-ul"></i>
+        <p>Toutes les catégories<p/>
+    </div>
+    ${CategoriesArray.map(
+      (category) =>
+        `
+      <div class='category__box__list__element'>
+        ${category.icon}
+        <p>${category.title}<p/>
+        <i class="fa-solid fa-chevron-right"></i>
+      </div>
+    `
+    ).join('')}
+  </div>
+<span>
+`
+
+// onclick add or remove
+categoriesBtn.addEventListener('click', () => {
+  // the button itself
+  const listBox = document.getElementById('category__box')
+  boxHandler(categoriesBtn, listHtml, listBox)
+})
+
+// box handling
+function boxHandler(refNode, insertElement, insertedElement) {
+  let categorysBtnCopy
+
+  if (!isOpen) {
+    refNode.insertAdjacentHTML('afterEnd', insertElement)
+    isOpen = true
+    categorysBtnCopy = document.getElementById('categoryBtnCopy')
+  } else {
+    closeBox(insertedElement)
+    return
+  }
+  // if we click on the copycat button
+  categorysBtnCopy.addEventListener('click', () => {
+    const listBox = document.getElementById('category__box')
+    closeBox(listBox)
+  })
+}
+
+// closebox
+function closeBox(box) {
+  box.remove()
+  isOpen = false
+}
